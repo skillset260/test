e7ed8715253dbfc8b290bb2b8b89c4485009764c
toshi yaduvanshi <toshi.yaduvanshi@codiotic.com>
2026-03-26 17:17:47 +0530

    add cron in server.js

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const http = require("http");
 const app = require("./app");
 const config = require("./config/config");
 const logger = require("./config/logger");
+const { autoExpirePlans } = require("./api/v1/cron/autoExpiredPlan");
 const port = config.port || 6000;
 const server = http.createServer(app);
+const cron = require("node-cron");
+
+// auto expire plans (12:05 AM)
+cron.schedule("5 0 * * *", async () => {
+  console.log("[CRON] Running autoExpirePlans...");
+  await autoExpirePlans();
+});
 
 //-------------------------------
 //start server
